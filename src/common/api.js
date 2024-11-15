12bdccda73b3c1f5a7bc4e67f00ffda5757f0a3d
ouyangxianghui001 <ouyangxianghui001>
2024-11-15 17:36:53 +0800

    接口集成

--- src/common/api.js
+++ src/common/api.js
@@ -9,4 +9,17 @@ export class API {
     static async getProductList(params, data) {
         return request(public_url + '/product/selectProductList', params, data, 'post')
     }
+    // /user/login   {"username":"admin","pwd":"111111"}
+    // data.name data.username data.token
+    static async login(params, data) {
+        return request(public_url + '/user/login', params, data, 'post')
+    }
+    // 新增编辑其他信息
+    static async addOrUpdateProperty(params, data) {
+        return request(public_url + '/product/addAndUpdateProperty', params, data, 'post')
+    }
+    // 新增编辑商品
+    static async addProduct(params, data) {
+        return request(public_url + '/product/add', params, data, 'post')
+    }
 }
